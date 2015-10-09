63d1b827f47503a56790611da4681a110683e805
Mihir Jham <mihirjham@gmail.com>
2015-10-08 18:03:39 -0700

    Phase 7: Incomplete

--- app/assets/javascripts/components/pokemons/form.js.jsx
+++ app/assets/javascripts/components/pokemons/form.js.jsx
@@ -3,6 +3,15 @@
   'use strict';
 
   var PokemonForm = root.PokemonForm = React.createClass({
-
+    mixins: [React.addons.LinkedStateMixin],
+    render: function(){
+      return(
+        <form>
+          Name:
+          <input valueLink={this.linkState("name")}/>
+          
+        </form>
+      );
+    }
   });
 }(this));
